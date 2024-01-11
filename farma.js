9564db4dabfa5c2c8d63597f021397dc182efb8a
GabrielfsCarrasco <152878089+GabrielfsCarrasco@users.noreply.github.com>
2024-01-11 14:53:12 -0300

    Add files via upload

---
+++ farma.js
@@ -0,0 +1,6 @@
+function funcao_a() {
+  document.getElementById("img").src="Screenshot_20231218_210554_Instagram.png";
+}
+function funcao_b() {
+  document.getElementById("farma").innerHTML="@_umafarmaceutica";
+}
